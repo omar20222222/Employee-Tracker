031ec709e7a7421fce2527c41b0ef8920b9a29a7
omar20222222 <108297683+omar20222222@users.noreply.github.com>
2022-12-14 11:27:48 +0000

    added new password to connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection(
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Abdullahi12",
     database: "employee_db",
   },
   console.log(`Connected to the employee-tracker database.`)
